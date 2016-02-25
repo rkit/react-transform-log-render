3dd7e12045efa26ca65cd74fe08a16cefb1a5c83
Igor Romanov <rkit.ru@gmail.com>
2016-02-26 00:11:33 +0700

    Add to ability include/exclude components (by name) #1

--- src/index.js
+++ src/index.js
@@ -8,20 +8,37 @@ function updateCountRender(componentId) {
   return count;
 }
 
-export default function logRender({ components }) {
+function inArray(componentName, list) {
+  return list.some((item) => {
+    if (componentName === item) {
+      return true;
+    }
+  });
+}
+
+export default function logRender({ components, imports }) {
+  const options = imports[0];
   return function wrap(ReactClass, componentId) {
     const originalRender = ReactClass.prototype.render;
     const displayName = components[componentId].displayName;
+    let enableLog = true;
 
-    ReactClass.prototype.render = function () {
-      const count = updateCountRender(componentId);
-      console.groupCollapsed(`render: ${displayName} (${count})`);
-      console.log('props', this.props);
-      console.log('state', this.state);
-      console.groupEnd();
+    if (options) {
+      enableLog = options.include ? enableLog && inArray(displayName, options.include) : enableLog;
+      enableLog = options.exclude ? enableLog && !inArray(displayName, options.exclude) : enableLog;
+    }
 
-      return originalRender.apply(this, arguments);
-    };
+    if (enableLog) {
+      ReactClass.prototype.render = function () {
+        const count = updateCountRender(componentId);
+        console.groupCollapsed(`render: ${displayName} (${count})`);
+        console.log('props', this.props);
+        console.log('state', this.state);
+        console.groupEnd();
+
+        return originalRender.apply(this, arguments);
+      };
+    }
 
     return ReactClass;
   };
